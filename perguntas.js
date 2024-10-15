25253c0769706c2e02d79fee1ab1c676324aac83
0miranha <143209180+0miranha@users.noreply.github.com>
2024-10-15 07:28:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'CIENCIAS',
+    'QUANTOS PLANETAS TEM O SISTEMA SOLAR?',
+    'OITO'
+)
+
+criaCartao(
+    'Geografia',
+    'O QUE SIGNIFICA ONU?',
+    'ORGANIZAÇAO DAS NAÇOES UNIDAS'
+)
+
+criaCartao(
+    'MATEMATICA',
+    'QUANTOS NUMEROS EXISTEM?',
+    'INFINITOS'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'QUAL O SOM DE SPEED?',
+    'I LONGO'
+)
